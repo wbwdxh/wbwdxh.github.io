d292e468a67d08655309201f4506b117f0c1e368
wbwdxh <wbwdxh>
2025-03-07 14:30:54 +0800

    sfddsfssd

--- auto_compile/CF2036E.md
+++ auto_compile/CF2036E.md
@@ -44,6 +44,7 @@
 	{
 		renderMathInElement(document.body, {
 			delimiters: [
+				{ left: '\\(', right: '\\)', display: true },
 				{ left: '$$', right: '$$', display: true },
 				{ left: '$', right: '$', display: false },
 			],
--- 转换.cpp
+++ 转换.cpp
@@ -172,6 +172,7 @@ signed main()
 	{
 		renderMathInElement(document.body, {
 			delimiters: [
+				{ left: '\\(', right: '\\)', display: true },
 				{ left: '$$', right: '$$', display: true },
 				{ left: '$', right: '$', display: false },
 			],
